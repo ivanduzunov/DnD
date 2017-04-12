a46964f1773ebe189ff4fc883a3c938d3608eff1
ivanduzunov <ivan.d.uzunov@gmail.com>
2017-04-12 21:16:57 +0300

    commit

--- DnD.Models/Room.cs
+++ DnD.Models/Room.cs
@@ -8,12 +8,14 @@ namespace DnD.Models
 {
     public class Room
     {
-       
+        public Room()
+        {
+            this.Dragons = new HashSet<Dragon>();
+        }
 
         public int Id { get; set; }
-
         public string Description { get; set; }
+        public virtual ICollection<Dragon> Dragons { get; set; }
 
-       
     }
 }
